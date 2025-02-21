33bc74c83e57e0ec1d07d1753a63343512012af2
Felipe Tavares <felipe.tavares@outlook.com>
2025-02-21 07:54:45 -0300

    receiving file in API

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -8,6 +8,12 @@ namespace DevFreela.API.Controllers
     {
         [HttpPost]
         public IActionResult Post() => Ok();
-        
+
+        [HttpPut("{id}/profile-picture")]
+        public IActionResult PostProfilePicture(IFormFile file)
+        {
+            var description = $"File: {file.FileName}, Size: {file.Length}";
+            return Ok(description);
+        }
     }
 }
