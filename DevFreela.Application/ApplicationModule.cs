dd23483ccc9e7cd448678b2e2e5be2382d3f248a
Felipe Tavares <felipe.tavares@outlook.com>
2025-08-07 08:33:14 -0300

    feat: implementing result pattern in application layer

---
+++ DevFreela.Application/ApplicationModule.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevFreela.Application;
+
+public static class ApplicationModule
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddService();
+        return services;
+    }
+        
+
+    private static IServiceCollection AddService(this IServiceCollection services)
+    {
+        services.AddScoped<IProjectService, ProjectService>();
+        return services;
+    }
+
+
+}
