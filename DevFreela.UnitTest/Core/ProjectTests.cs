922ab5e99201e454df890d73f1dffb3f8ce64f1a
Felipe Tavares <felipe.tavares@outlook.com>
2025-10-22 20:36:46 -0300

    test: add tests for Project entity

---
+++ DevFreela.UnitTest/Core/ProjectTests.cs
@@ -0,0 +1,80 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTest.Core;
+
+public class ProjectTests
+{
+    [Fact]
+    public void ProjectIsCreated_Start_Success()
+    {
+        // Arrange
+        var project = new Project("Project", "Project Description", 1, 2, 1000);
+
+        // Act
+        project.Start();
+
+        // Assert
+        Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+        Assert.NotNull(project.StartedAt);
+
+        Assert.True(project.Status == ProjectStatusEnum.InProgress);
+        Assert.False(project.StartedAt is null);
+    }
+
+    [Fact]
+    public void ProjectIsInInvalidState_Start_ThrowsException()
+    {
+        // Arrange
+        var project = new Project("Project", "Project Description", 1, 2, 1000);
+        project.Start();
+
+        // Act + Assert
+        Action start = project.Start;
+
+        var exception = Assert.Throws<InvalidOperationException>(start);
+        Assert.Equal(Project.INVALID_STATE_MESSAGE, exception.Message);
+    }
+
+    [Fact]
+    public void ProjectUpdated_ShouldReturn_Success()
+    {
+        // Arrange
+        var project = new Project("Project", "Project Description", 1, 2, 1000);
+
+        // Act
+        var titleExpected = "Title";
+        var descriptionExpected = "Description";
+        var totalCostExpected = 1500;
+        project.Update(titleExpected, descriptionExpected, totalCostExpected);
+
+        // Assert
+        Assert.Equal(project.Title, titleExpected);
+        Assert.Equal(project.Description, descriptionExpected);
+        Assert.Equal(project.TotalCost, totalCostExpected);
+    }
+
+    [Fact]
+    public void ProjectIsInPaymentPending_Complete_Success()
+    {
+        // Arrange
+        var project = new Project("Project", "Project Description", 1, 2, 1000);
+
+        // Act
+        project.Start();
+        project.SetPaymentPending();
+        project.Complete();
+
+        // Assert
+        Assert.Equal(ProjectStatusEnum.Completed, project.Status);
+        Assert.NotNull(project.CompletedAt);
+
+        Assert.True(project.Status == ProjectStatusEnum.Completed);
+        Assert.False(project.CompletedAt is null);
+    }
+}
